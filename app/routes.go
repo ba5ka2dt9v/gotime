c2f82f3d413c019c9aa5abc34eb88e430fded9ca
Jonathan Rawlinson <jrawlinson@sales-i.com>
2022-04-28 13:58:04 +0100

    added tz parameter to select a time zone

--- app/routes.go
+++ app/routes.go
@@ -1,11 +1,34 @@
 package app
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 )
 
+type TimeResponse struct {
+	CurrentTime string `json:"current_time"`
+}
+
 func getTime(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, time.Now().Format(time.RFC3339))
+	if loc, err := LoadLocation(r); loc != nil {
+		w.Header().Add("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(TimeResponse{CurrentTime: time.Now().In(loc).Format(time.RFC3339)}); err != nil {
+			log.Panic(err)
+		}
+	} else {
+		log.Println(err)
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("invalid timezone"))
+	}
+}
+
+func LoadLocation(r *http.Request) (*time.Location, error) {
+	values := r.URL.Query()
+	tz := "UTC"
+	if values.Has("tz") {
+		tz = values.Get("tz")
+	}
+	return time.LoadLocation(tz)
 }
--- app/start.go
+++ app/start.go
@@ -9,6 +9,6 @@ import (
 
 func Start() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", getTime).Methods(http.MethodGet)
+	r.HandleFunc("/api/time", getTime).Methods(http.MethodGet)
 	log.Fatal(http.ListenAndServe("localhost:8000", r))
 }
